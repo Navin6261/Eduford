b00bae786487ef5401ad5a36af63b3e67ce7e334
Navin Turkar <68228993+Navin6261@users.noreply.github.com>
2022-04-01 22:55:13 +0530

    Add files via upload

---
+++ eduford/form_handler.php
@@ -0,0 +1,13 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$comment = $_POST['comment'];
+
+$email_from = 'navintrukar@gmail.com'
+
+$email_subject = 'New Form Submission'
+
+$email_body = "User Name: $name.\n ".
+
+
+?>
